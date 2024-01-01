d5d49c47761f6cc91ceb808d951c493e133e4bf7
CiKlopen <1366230318@qq.com>
2024-01-01 20:08:04 +0800

    Create app.py

---
+++ API/app.py
@@ -0,0 +1,14 @@
+#python  api 
+#依赖flask库
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def hello_world():
+    return 'hello,world'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=7588)
