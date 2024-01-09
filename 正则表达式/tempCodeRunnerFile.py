a163ba13f87ea394f1c55089a6d11413de162bfc
CiKlopen <1366230318@qq.com>
2024-01-09 15:13:54 +0800

    Create tempCodeRunnerFile.py

---
+++ 正则表达式/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+import re
+
+# def validate_string(input_string):
+#     pattern = r'^[01]+$'  # 正则表达式，表示字符串只能包含0和1
+#     match = re.match(pattern, input_string)
+
+#     if match:
+#         print(f"字符串 '{input_string}' 符合正则表达式。")
+#     else:
+#         print(f"字符串 '{input_string}' 不符合正则表达式。")
+
+# # 测试字符串
+# test_string = "110010"
+# validate_string(test_string)
